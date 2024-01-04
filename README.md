3e14cee14a544886d6838c09101e1d0fbef57213
Reuben Cleetus <rcleetus@gmail.com>
2024-01-04 13:54:40 -0500

    Added Gemini support

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 ## SQL-GPT
-This repository is a demo of using OpenAI GPT 3.5 to reason over a SQLite database. Ask queries in English or any other langaage supported by GPT-3 and it generates the corresponding SQL and gets the results from the database.
+This repository is a demo of using OpenAI GPT 3.5 to reason over a SQLite database -- And now adding Google Gemini as well! Ask queries in English or any other langaage supported by GPT-3 and it generates the corresponding SQL and gets the results from the database.
+
+Please see the [readme in this repo](https://github.com/marlinspike/gemini_chat_console) for instructions on how to get your Gemini API key if you want to try it out.
 
 ## Getting Started
 Get an OpenAI API key, and create a file called **.env** with the following contents:
 ```
 OPENAI_API_KEY="sk-..."
+GEMINI_API_KEY="..."
 ```
 
 Then install the dependencies:
@@ -14,8 +17,10 @@ pip install -r requirements.txt
 
 Then run the following command:
 ```
-python app.py
+python app.py openai
+python app.py gemini
 ```
+
 
 ## Demo of a Legacy Application interacting with a Generative AI app
 This demonstrates a legacy application interacting with a generative AI application. The legacy application is a simple web application that gets an XML payload from the driver app. The Generative AI driver app is a RESTful app, which speaks in JSON, but uses Generative AI to translate that JSON into XML for the Legacy App.
--- app.py
+++ app.py
@@ -1,20 +1,64 @@
 import sqlite3
-import openai
+import sys
+from abc import ABC, abstractmethod
+from dotenv import load_dotenv
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
-from dotenv import load_dotenv
 from langchain.prompts.chat import ChatPromptTemplate
 from rich.console import Console
 from rich.table import Table
+from rich.console import Console
+from rich.panel import Panel
+from rich.text import Text
 from utils import DB_SCHEMA
+import google.generativeai as genai
+import os
 
+AI_MODEL_NAME=""
 load_dotenv()
 console = Console()
-chat = ChatOpenAI(temperature=0, verbose=True)
 
+# Abstract class for AI models
+class AIModel(ABC):
+    @abstractmethod
+    def generate_response(self, input_text):
+        pass
 
-# Function to translate English to SQL using GPT-4
-def english_to_sql(english_input):
+# Concrete implementation for OpenAI
+class OpenAIModel(AIModel):
+    def __init__(self):
+        self.chat = ChatOpenAI(temperature=0, verbose=True)
+
+    def generate_response(self, input_text):
+        obj = self.chat(input_text)
+        return obj.content
+
+# Concrete implementation for Gemini
+class GeminiModel(AIModel):
+    def __init__(self):
+        genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+        self.model = genai.GenerativeModel(model_name = "gemini-pro")
+    
+    def generate_response(self, input_text):
+        obj = self.model.generate_content(input_text)
+        val = obj.text.replace("```sql", "").replace("```","")
+        return val
+
+def print_model_banner(text: str):
+    console = Console()
+    text = Text(f"{text}", style="bold")
+    text.justify = "center"  # Align the text
+
+    # Create a Panel with the aligned text
+    panel = Panel(text, width=40, expand=False)
+    console.print(panel)
+
+# Function to check if the string is a valid SQL query
+def is_valid_sql(query):
+    return "SELECT" in query or "INSERT" in query or "UPDATE" in query or "DELETE" in query
+
+# Function to translate English to SQL using AI Model
+def english_to_sql(english_input, ai_model):
     sql_translate_string = f"""You are a helpful SQL assistant that translates Text written in conversational text to SQL. You must always have a column name, by Aliasing a column to something meaningful if needed. You MUST ONLY return the SQL with nothing else. 
     - This is a SQLite database, so use appropriate SQLite syntax.
     - Use your best judgement to include columns that are relevant to the question.
@@ -24,58 +68,65 @@ def english_to_sql(english_input):
 
     >>> Here's the Text: {{input}}"""
 
-    sql_translate_template = ChatPromptTemplate.from_template(sql_translate_string)
-    sql_translate_messages = sql_translate_template.format_messages(input = english_input)
-    
-    # Use Langchain to send the prompt to GPT-4
-    #llm = OpenAI(temperature=0, verbose=True)
-    response = chat(sql_translate_messages)
-    sql_query = response.content
-    return sql_query
+    if (AI_MODEL_NAME == "openai"):
+        sql_translate_template = ChatPromptTemplate.from_template(sql_translate_string)
+        sql_translate_messages = sql_translate_template.format_messages(input=english_input)
+    elif (AI_MODEL_NAME == "gemini"):
+        fmt = sql_translate_string.replace("{input}", english_input).replace("{DB_SCHEMA}", DB_SCHEMA)
+        sql_translate_messages = fmt.replace("```","")
 
+
+    response = ai_model.generate_response(sql_translate_messages)
+    print(response)
+    sql_query = response
+
+    if is_valid_sql(sql_query):
+        return sql_query, True
+    else:
+        return response, False #.content
 
 # Function to execute SQL against the Chinook database
 def execute_sql(sql_query):
-    # Connect to the Chinook database
     conn = sqlite3.connect("Chinook.db")
     cursor = conn.cursor()
-    
-    # Execute the query and fetch the results
-    #res = cursor.execute(sql_query)
     cursor.execute(sql_query)
-    results = cursor.fetchall()  # Fetch all results
-    column_names = [description[0] for description in cursor.description]  # Get column names
-    
+    results = cursor.fetchall()
+    column_names = [description[0] for description in cursor.description]
     return results, column_names
 
 # Main application loop
 def main():
+    global AI_MODEL_NAME
+    AI_MODEL_NAME = sys.argv[1] if len(sys.argv) > 1 else "openai"
+    if AI_MODEL_NAME == "openai":
+        ai_model = OpenAIModel()
+    elif AI_MODEL_NAME == "gemini":
+        ai_model = GeminiModel()
+    # Add more conditions for different AI models here
+    else:
+        raise ValueError("Unsupported AI model")
+
+    print_model_banner(f"AI Model: {AI_MODEL_NAME}")
+
     while True:
-        # Take user input
-        user_input = input("\nEnter your query (or type 'exit' to quit): ")
+        user_input = input(f"\n{AI_MODEL_NAME}: Enter your query (or type 'exit' to quit): ")
         if user_input.lower() == 'exit':
             break
-        
-        # Translate to SQL
-        try:
-            sql_query = english_to_sql(user_input)
-            print(f"SQL Query: {sql_query}")
-            
-            # Execute SQL and print results
-            results, col_names = execute_sql(sql_query)
-            table = Table(show_header=True, header_style="bold magenta")
-            # Add columns to the table
-            for column_name in col_names:
-                table.add_column(column_name)
 
-            for row in results:
-                table.add_row(*[str(item) for item in row])
-                #print(row)
-            console.print(table)
-            print()
-        except Exception as e:
-            print(f"An error occurred: {e}")
-
+        sql_query, is_valid = english_to_sql(user_input, ai_model)
+        if is_valid:
+            try:
+                results, col_names = execute_sql(sql_query)
+                table = Table(show_header=True, header_style="bold magenta")
+                for column_name in col_names:
+                    table.add_column(column_name)
+                for row in results:
+                    table.add_row(*[str(item) for item in row])
+                console.print(table)
+            except Exception as e:
+                print(f"An error occurred while executing SQL: {e}")
+        else:
+            print(f"Non-SQL Response: {sql_query}")
 
 if __name__ == "__main__":
     main()
